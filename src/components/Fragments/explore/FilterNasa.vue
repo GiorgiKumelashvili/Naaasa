d052edd06ef6ad6bcf35983aae899e6000f1057b
GiorgiKumelashvili <giorgi.kumelashvili21@gmil.com>
2021-01-13 00:39:12 +0400

    same message as down --- (↓)

--- src/components/Fragments/explore/FilterNasa.vue
+++ src/components/Fragments/explore/FilterNasa.vue
@@ -1,117 +1,128 @@
 <template>
-	<v-card class="mx-auto" max-width="300">
-		<v-card-text>
-			<!-- Media Type -->
-			<div>
-				<v-card-subtitle class="pa-0 py-1 text-body-1 grey--text text--darken-4">
-					Media Type
-				</v-card-subtitle>
+    <v-card class="mx-auto" max-width="300">
+        <v-card-text>
+            <!-- Media Type -->
+            <div>
+                <v-card-subtitle
+                    class="pa-0 py-1 text-body-1 grey--text text--darken-4"
+                >
+                    Media Type
+                </v-card-subtitle>
 
-				<!-- media type -->
-				<v-chip-group
-					v-model="coreFilter.selectedMediaType"
-					@change="setFinalMediaType($event)"
-					column
-					multiple
-				>
-					<v-chip filter outlined label>
-						Images
-					</v-chip>
-					<v-chip filter outlined label>
-						Videos
-					</v-chip>
-				</v-chip-group>
-			</div>
+                <!-- media type -->
+                <v-chip-group
+                    v-model="coreFilter.selectedMediaType"
+                    @change="setFinalMediaType($event)"
+                    column
+                    multiple
+                >
+                    <v-chip filter outlined label>
+                        Images
+                    </v-chip>
+                    <v-chip filter outlined label>
+                        Videos
+                    </v-chip>
+                </v-chip-group>
+            </div>
 
-			<!-- Year Interval-->
-			<div>
-				<v-card-subtitle class="pa-0 pt-5 pb-1 text-body-1 grey--text text--darken-4">
-					Year Interval
-				</v-card-subtitle>
+            <!-- Year Interval-->
+            <div>
+                <v-card-subtitle
+                    class="pa-0 pt-5 pb-1 text-body-1 grey--text text--darken-4"
+                >
+                    Year Interval
+                </v-card-subtitle>
 
-				<v-range-slider v-model="coreFilter.range" :max="coreFilter.max" :min="coreFilter.min" />
+                <v-range-slider
+                    v-model="coreFilter.range"
+                    :max="coreFilter.max"
+                    :min="coreFilter.min"
+                />
 
-				<div class="d-flex">
-					<v-select
-						v-model="coreFilter.range[0]"
-						@change="$set(coreFilter.range, 0, $event)"
-						:items="coreFilter.years"
-						menu-props="auto"
-						label="Solo field"
-						dense
-						solo
-					/>
-					<v-select
-						v-model="coreFilter.range[1]"
-						@change="$set(coreFilter.range, 1, $event)"
-						:items="coreFilter.years"
-						menu-props="auto"
-						label="Solo field"
-						hide-details
-						single-line
-						dense
-						solo
-						class="pl-5"
-					/>
-				</div>
-			</div>
-		</v-card-text>
+                <div class="d-flex">
+                    <v-select
+                        v-model="coreFilter.range[0]"
+                        @change="$set(coreFilter.range, 0, $event)"
+                        :items="coreFilter.years"
+                        menu-props="auto"
+                        label="Solo field"
+                        dense
+                        solo
+                    />
+                    <v-select
+                        v-model="coreFilter.range[1]"
+                        @change="$set(coreFilter.range, 1, $event)"
+                        :items="coreFilter.years"
+                        menu-props="auto"
+                        label="Solo field"
+                        hide-details
+                        single-line
+                        dense
+                        solo
+                        class="pl-5"
+                    />
+                </div>
+            </div>
+        </v-card-text>
 
-		<v-card-actions>
-			<v-spacer />
+        <v-card-actions>
+            <v-spacer />
 
-			<v-btn text @click="$emit('cancelFilter')">
-				Cancel
-			</v-btn>
+            <v-btn text @click="$emit('cancelFilter')">
+                Cancel
+            </v-btn>
 
-			<v-btn color="indigo" text>
-				Filter
-			</v-btn>
-		</v-card-actions>
-	</v-card>
+            <v-btn
+                color="indigo"
+                @click="$emit('cancelFilterAndFilter', final)"
+                text
+            >
+                Filter
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
 export default {
-	data: () => ({
-		// Core functionalities
-		coreFilter: {
-			// media type
-			selectedMediaType: [0],
-			imageVal: 0,
-			videoVal: 1,
+    data: () => ({
+        // Core functionalities
+        coreFilter: {
+            // media type
+            selectedMediaType: [0],
+            imageVal: 0,
+            videoVal: 1,
 
-			// year interval
-			min: 1920,
-			max: 2021,
-			years: [].range(1920, new Date().getFullYear()),
-			range: [-1920, new Date().getFullYear()]
-		},
+            // year interval
+            min: 1920,
+            max: 2021,
+            years: [].range(1920, new Date().getFullYear()),
+            range: [-1920, new Date().getFullYear()]
+        },
 
-		// Final filter
-		final: {
-			mediaType: {
-				image: true,
-				video: false
-			},
-			yearInterval: {
-				start: 1920,
-				end: new Date().getFullYear()
-			}
-		}
-	}),
-	methods: {
-		setFinalMediaType: function(arr) {
-			this.final.mediaType.image = arr.includes(this.coreFilter.imageVal) ? true : false
-			this.final.mediaType.video = arr.includes(this.coreFilter.videoVal) ? true : false
-		}
-		// [End] setFinalMediaType
-	},
-	watch: {
-		'coreFilter.range': function() {
-			this.final.yearInterval.start = this.coreFilter.range[0]
-			this.final.yearInterval.end = this.coreFilter.range[1]
-		}
-	}
-}
+        final: {
+            image: true,
+            video: false,
+            year_start: 1920,
+            year_end: new Date().getFullYear()
+        }
+    }),
+    methods: {
+        setFinalMediaType: function(arr) {
+            this.final.image = arr.includes(this.coreFilter.imageVal)
+                ? true
+                : false;
+            this.final.video = arr.includes(this.coreFilter.videoVal)
+                ? true
+                : false;
+        }
+        // [End] setFinalMediaType
+    },
+    watch: {
+        "coreFilter.range": function() {
+            this.final.year_start = this.coreFilter.range[0];
+            this.final.year_end = this.coreFilter.range[1];
+        }
+    }
+};
 </script>
--- src/libs/VueGlobal.js
+++ src/libs/VueGlobal.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
-import Back from '@/libs/Back';
-import axios from 'axios';
-import Config from './Config';
+import Vue from "vue";
+import Back from "@/libs/Back";
+import axios from "axios";
+import Config from "./Config";
 
 let { keys } = Config;
 
@@ -10,27 +10,63 @@ Vue.prototype.$service = (methodNum, obj) => Back.Service(methodNum, obj);
 
 // Front api
 Vue.prototype.$nasa = {
-    // NASA Image and Video Library
-    async img_video_lib(params) {
-        const BASE = 'https://images-api.nasa.gov/';
+    /**
+     * NASA Image and Video Library
+     * @param {Object} conf
+     * @returns {Object} response
+     * @description:
+     *      manipulate api for request to nasa
+     *      image and video library
+     *
+     */
+    async img_video_lib(conf) {
+        // check if object exists
+        if (!conf && typeof conf === "object")
+            throw new Error("[GIO] Invalid configuration object");
 
-        let query = `${BASE}search?q=Earth&media_type=image,video&page=1&year_start=2020&year_end=2021`;
-        let response = await axiosGet(query);
+        // base url for api
+        const BASE = "https://images-api.nasa.gov/search";
 
-        if (response) {
-            let { data: { collection } } = response
-            console.log(collection);
-        }
+        // destructure
+        const { query, video, image, year_start, year_end } = conf;
+
+        // manipulate parameters for api req
+        let final_query = query ? query : "Earth";
+        let media = `&media_type=${image ? "image" : ""}${
+            video && image ? "," : ""
+        }${video ? "video" : ""}`;
+        let page = "&page=1";
+        let year_s = `&year_start=${year_start ? year_start : "1920"}`;
+        let year_e = `&year_end=${
+            year_end ? year_end : new Date().getFullYear()
+        }`;
+        let requestText = `${BASE}?q=${final_query}${media}${page}${year_s}${year_e}`;
+
+        console.log(requestText);
+        // get response
+        let response = await axiosGet(requestText);
+
+        // return result
+        return response.data.collection.items || null;
     },
 
-    // Astronomy Picture of the Day
+    /**
+     * Astronomy Picture of the Day (aka APOD)
+     *
+     * @param {null}
+     * @returns {Object} response
+     * @description:
+     *      manipulate api for request to nasa
+     *      image and video lib
+     *
+     */
     async nasa_apod() {
         let query = `https://api.nasa.gov/planetary/apod?api_key=${keys.NASA_KEY}`;
         let respone = await axiosGet(query);
 
         return respone ? respone.data : null;
     }
-}
+};
 
 async function axiosGet(url) {
     try {
